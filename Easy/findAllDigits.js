423b5afca6ff4fd158d85a757b2a4bd53608bccb
Tom <a.dybovsky@gmail.com>
2021-02-17 09:48:38 +0200

    factorchain, findDigit, findNemo

--- Easy/findAllDigits.js
+++ Easy/findAllDigits.js
@@ -15,8 +15,48 @@ Notes
 The digits can be discovered in any order.
 */
 
-function findAllDigits( /*args*/ ) {
-  //your code
+function findAllDigits(arr) {
+  let newArr = arr.map(String);
+  console.log(newArr);
+  let counterArr = [];
+  for (let i = 0; i <= newArr.length - 1; i++) {
+    for (let j = 0; j <= newArr[i].length - 1; j++) {
+      if (!counterArr.includes(newArr[i][j])) {
+        counterArr.push(newArr[i][j]);
+        if (counterArr.length === 10) {
+          newArr = arr.map(Number);
+          return newArr[i];
+        }
+      }
+    }
+  }
+  return "Missing digits!";
 }
+
+// //your code
+
+// function findAllDigits(array /*args*/) {
+//   let templArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+//   for (let i = 0; i < array.length; i++) {
+//     for (let j = 0; j <= 10; j++) {
+//       if (String(array[i]).includes(String(templArray[j]))) {
+//         templArray.splice(templArray.indexOf(templArray[j]), 1);
+
+//         if (templArray.length === 0) {
+//           return array[i];
+//         }
+//       }
+
+//       //console.log(typeof array[i], typeof templArray[j])
+//     }
+//   }
+//   return "Missing digits!";
+// }
+
+// console.log(findAllDigits([2758,3737,3349,5118,7004,6106,8868,6687,2510,8911])) //6106
+// console.log(findAllDigits([3914,9923,8187,1657,4271,9538,3759,4543,3438,1943]))
+// console.log(findAllDigits([8452,7326,6786,1893,6546,8714,6699,1361,4891,9797]))
+// console.log(findAllDigits([3129,7689,7449,4389,5855,9670,9245,1291,7367,1810]))//9670
+// console.log(findAllDigits([7985,7130,4625,7392,4933,7163,7130,2145,1596,6188]))//4625
 
 exports.solution = findAllDigits;
--- Easy/findNemo.js
+++ Easy/findNemo.js
@@ -15,9 +15,23 @@ Nemo will always look like Nemo, and not NeMo or other capital variations.
 Nemo's, or anything that says Nemo with something behind it, doesn't count as Finding Nemo.
 If there are multiple Nemo's in the sentence, only return for the first one.
 */
+function findNemo(string /*args*/) {
+  let myArr = string.split(" ");
 
-function findNemo( /*args*/ ) {
-  //your code
+  for (let i = 0; i < myArr.length; i++) {
+    console.log(myArr[i]);
+    if (myArr[i] === "Nemo") {
+      return `I found Nemo at ${myArr.indexOf(myArr[i]) + 1}!`; //your code
+    }
+  }
+  return "I can't find Nemo :(";
 }
+
+// function findNemo(string /*args*/) {
+//   let myArr = string.split(" ");
+
+//   let res = myArr.find((el) => el === "Nemo");
+//   return `I found Nemo at ${myArr.indexOf(res) + 1}!`; //your code
+// }
 
 exports.solution = findNemo;
